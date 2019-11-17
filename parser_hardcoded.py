c695c54d102cacb67560ebeb324b0a28ada775c9
Gabrielle <54288922+gabriellekc@users.noreply.github.com>
2019-11-17 17:21:10 -0500

    Add files via upload

---
+++ parser_hardcoded.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+mat = np.load("insert-file-name-here.npy")
+
+xs = [row[0] for row in mat]
+ys = [row[1] for row in mat]
+zs = [row[2] for row in mat]
+mat = np.asarray([xs,ys,zs]).T
+
+# filtering - currently hardcoded
+mat = mat[(mat[:,0]>=0.5) | (mat[:,0]<=-0.5)] # x-axis
+mat = mat[(mat[:,1]>=1) | (mat[:,1]<=-1)] # y-axis
+mat = mat[(mat[:,2]<1.5) & (mat[:,2]>-1.5)] # z-axis
+
+# 3D visualization
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.scatter(mat[:,0],mat[:,1],mat[:,2],c=mat[:,2])
+plt.show()
