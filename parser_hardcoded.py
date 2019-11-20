9d75310a0f25265e6cc594a239603cccc617dec4
Gabrielle <54288922+gabriellekc@users.noreply.github.com>
2019-11-20 17:03:50 -0500

    Fix LIDAR filtering

--- parser_hardcoded.py
+++ parser_hardcoded.py
@@ -10,12 +10,19 @@ zs = [row[2] for row in mat]
 mat = np.asarray([xs,ys,zs]).T
 
 # filtering - currently hardcoded
+mat = mat[(mat[:,2]<1.5) & (mat[:,2]>-1.5)] # z-axis
 mat = mat[(mat[:,0]>=0.5) | (mat[:,0]<=-0.5)] # x-axis
 mat = mat[(mat[:,1]>=1) | (mat[:,1]<=-1)] # y-axis
-mat = mat[(mat[:,2]<1.5) & (mat[:,2]>-1.5)] # z-axis
 
 # 3D visualization
 fig = plt.figure()
 ax = fig.add_subplot(111,projection='3d')
 ax.scatter(mat[:,0],mat[:,1],mat[:,2],c=mat[:,2])
+plt.show()
+
+# 2D for planning
+grid = np.column_stack((mat[:,0],mat[:,1]))
+
+fig = plt.figure()
+plt.scatter(mat[:,0],mat[:,1])
 plt.show()
